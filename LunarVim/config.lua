8aac504eb60f5814ee118ddf3751018a41ac96f8
Eduard Korchmar <eduard.korchmar@odysseusinc.com>
2024-03-22 13:44:56 +0100

    Add windows specific config

--- LunarVim/config.lua
+++ LunarVim/config.lua
@@ -130,4 +130,29 @@ require('colorizer').setup()
 -- Toggleterm
 vim.keymap.set({'v', 'n'}, '<leader>t', '<cmd>lua require("toggleterm").toggle()<cr>')
 
+-- Windows specific
+if vim.fn.has('win32') then
+
+  -- Enable powershell as your default shell
+  vim.opt.shell = "pwsh.exe -NoLogo"
+  vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.cmd [[
+      let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+      set shellquote= shellxquote=
+    ]]
+
+  -- Set a compatible clipboard manager
+  vim.g.clipboard = {
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+  }
+end
 
