f404dede570a9efb79388bda55858f9691af4c51
Eduard Korchmar <eduard.korchmar@odysseusinc.com>
2024-05-22 09:51:59 +0200

    Change to Vi mode and override cd with zoxide

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,5 +4,12 @@ $ENV:COLORTERM = "truecolor"
 Invoke-Expression (&starship init powershell)
 
 Set-Alias lvim "$HOME\.local\bin\lvim.ps1"
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+$ENV:_ZO_ECHO = "1"
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineKeyHandler -Chord Tab -Function Complete
+Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Insert
+Set-PSReadLineKeyHandler -Chord Ctrl-r -Function ReverseSearchHistory -ViMode Command
 
