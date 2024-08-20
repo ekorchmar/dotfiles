b7572aa432d48048f18e8995c8f00c05b9d95d52
Eduard Korchmar <eduard_korchmar@epam.com>
2024-08-20 09:38:33 +0200

    direnv support for windows

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,8 @@ $ENV:FZF_DEFAULT_OPTS=@"
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
 "@
 
+$env:HOME = "$env:HOMEPATH"
+
 Invoke-Expression (&starship init powershell)
 
 Set-Alias lvim "$HOME\.local\bin\lvim.ps1"
@@ -250,3 +252,6 @@ filter __gh_escapeStringWithSpecialChars {
 }
 
 Register-ArgumentCompleter -CommandName 'gh' -ScriptBlock $__ghCompleterBlock
+
+Invoke-Expression "$(direnv hook pwsh)"
+
