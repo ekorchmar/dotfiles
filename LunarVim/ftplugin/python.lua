d18ab8a44fbf0c8a0bd71d3c0889d701b2a1fac2
Eduard Korchmar <korchmar.eduard@gmail.com>
2025-03-12 23:09:33 +0100

    fix: revert switching to stricter typechecker

--- LunarVim/ftplugin/python.lua
+++ LunarVim/ftplugin/python.lua
@@ -1,3 +1,3 @@
 -- require("lvim.lsp.manager").setup("pylsp")
 require("lvim.lsp.manager").setup("ruff")
-require("lvim.lsp.manager").setup("pyright")
+require("lvim.lsp.manager").setup("basedpyright")
