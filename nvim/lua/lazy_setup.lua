fbad367477ba91010b0d8e6f9d3831c4e321e5c4
Eduard Korchmar <korchmar.eduard@gmail.com>
2025-07-16 22:41:08 +0200

    feat: do not unload plugins

--- nvim/lua/lazy_setup.lua
+++ nvim/lua/lazy_setup.lua
@@ -18,15 +18,5 @@ require("lazy").setup({
   install = { colorscheme = { "astrotheme", "habamax" } },
   ui = { backdrop = 100 },
   performance = {
-    rtp = {
-      -- disable some rtp plugins, add more to your liking
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "zipPlugin",
-      },
-    },
   },
 } --[[@as LazyConfig]])
