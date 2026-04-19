eb9cf9a1a967f04b60c191843af5e8798972148c
Eduard Korchmar <korchmar.eduard@gmail.com>
2026-04-19 22:20:56 +0200

    fix(nvim): remove unused and duplicated mappings

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -83,29 +83,6 @@ return {
           function() require("astrocore.buffer").nav(-vim.v.count1) end,
           desc = "Previous buffer",
         },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
-        ["<Leader>bq"] = {
-          function()
-            local buf = require "astrocore.buffer"
-            require("astroui.status.heirline").buffer_picker(function() buf.close(buf.current_buf) end)
-          end,
-          desc = "Close current buffer",
-        },
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
       },
     },
   },
